9992f9c865297c4075fb1e7ee9b22cadc0b4a4cd
shashif <shoriful.hasan@gmail.com>
2023-05-19 00:25:39 +0600

    forget password Done

--- lib/screens/forget_password/Forget_Password.dart
+++ lib/screens/forget_password/Forget_Password.dart
@@ -1,11 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../dialogbox/error_alert_dialog.dart';
+import '../splash_screen/splash_screen.dart';
+import 'forget_background.dart';
+
 class ForgetPassword extends StatelessWidget {
+  TextEditingController emailController = TextEditingController();
+  final FirebaseAuth firebaseAuth=FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Forget Password Screen'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: emailController,
+          ),
+          SizedBox(height: 20,),
+          MaterialButton(child: Text('Reset now'), onPressed: () async{
+
+            try{
+              await firebaseAuth.sendPasswordResetEmail(email: emailController.text);
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+            }
+            catch(error){
+              ErrorAlertDialog(message:error.toString() ,);
+            }
+          }),
+        ]
       ),
     );
   }
